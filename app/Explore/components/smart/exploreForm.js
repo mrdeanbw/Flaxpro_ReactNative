4fcf312a9e8665b1c7a61ac02503d35512911f33
danielfred1129 <alex@Alexs-iMac.local>
2017-02-16 20:01:02 +0300

    Explor page - added gym feature.

--- app/Explore/components/smart/exploreForm.js
+++ app/Explore/components/smart/exploreForm.js
@@ -15,8 +15,11 @@ import {
 import MapView from 'react-native-maps';
 import DatePicker from 'react-native-datepicker';
 import Icon from 'react-native-vector-icons/EvilIcons';
+import PopupDialog from 'react-native-popup-dialog';
+
 import { SegmentedControls } from 'react-native-radio-buttons';
 import SearchBar from './searchBar';
+
 
 const ASPECT_RATIO = width / height;
 const LATITUDE = 37.78825;
@@ -37,6 +40,35 @@ export default class ExploreForm extends Component {
 
     this.state = {
       selectedSegmented : 'ALL',
+      region: {
+        latitude: LATITUDE,
+        longitude: LONGITUDE,
+        latitudeDelta: LATITUDE_DELTA,
+        longitudeDelta: LONGITUDE_DELTA,
+      },
+      markers: [
+        {
+          coordinate: {
+            latitude: LATITUDE + SPACE,
+            longitude: LONGITUDE + SPACE,
+          },
+          key: 0,
+        },
+        {
+          coordinate: {
+            latitude: LATITUDE + SPACE,
+            longitude: LONGITUDE - SPACE,
+          },
+          key: 1,
+        },
+        {
+          coordinate: {
+            latitude: LATITUDE,
+            longitude: LONGITUDE,
+          },
+          key: 2,
+        },
+      ],
     };
   }
 
@@ -59,8 +91,22 @@ export default class ExploreForm extends Component {
     Alert.alert('Clicked onFilter');
   }
 
+  onPressPin(key) {
+    this.popupDialog.openDialog();
+  }
+
+  onCloseGym () {
+
+    this.popupDialog.closeDialog();
+  }
+  onSetGym () {
+
+    this.popupDialog.closeDialog();
+  }
+
   render() {
     const { status } = this.props;
+    const { region, markers } = this.state;
 
     return (
       <View style={ styles.container }>
@@ -68,12 +114,12 @@ export default class ExploreForm extends Component {
           <View style={ styles.searchBarWrap }>
             <SearchBar
               onSearchChange={() => console.log('On Focus')}
-              height={25}
+              height={ 25 }
               autoCorrect={ false }
               returnKeyType={ "search" }
               iconColor={ "#ffffff99" }
               placeholderColor="#ffffff99"
-              paddingTop={20}
+              paddingTop={ 20 }
             />
           </View>
           <View style={ styles.calendarBarWrap } >
@@ -128,36 +174,52 @@ export default class ExploreForm extends Component {
               }}
             />
           </View>
+
           <MapView
             style={ styles.map }
-            initialRegion={{
-              latitude: LATITUDE,
-              longitude: LONGITUDE,
-              latitudeDelta: LATITUDE_DELTA,
-              longitudeDelta: LONGITUDE_DELTA,
-            }}
+            initialRegion={ region }
           >
-            <MapView.Marker
-              image={ pin_gym }
-              coordinate={{
-                latitude: LATITUDE,
-                longitude: LONGITUDE,
-              }}
-              >
-
-            </MapView.Marker>
-
-            <MapView.Marker
-              image={ pin_gym }
-              coordinate={{
-                latitude: LATITUDE - SPACE * 2,
-                longitude: LONGITUDE + SPACE,
-              }}
-            >
-
-            </MapView.Marker>
-
+            {
+              this.state.markers.map(marker => (
+                <MapView.Marker
+                  image={ pin_gym }
+                  key={ marker.key }
+                  coordinate={ marker.coordinate }
+                  calloutOffset={{ x: 0, y: 28 }}
+                  calloutAnchor={{ x: 0.5, y: 0.4 }}
+                  onPress={ () => this.onPressPin(marker.key) }
+                >
+                </MapView.Marker>
+              ))
+            }
           </MapView>
+          <PopupDialog
+            ref={(popupDialog) => { this.popupDialog = popupDialog; }}
+            width={ width * 0.8 }
+            dialogStyle={ styles.dialogContainer }
+          >
+            <View style={ styles.dialogMainContentContainer }>
+              <View style={ styles.dialogContentContainer }>
+                <Text style={ styles.dialogText }>Do you want to set this Gym as the prefered workout location?</Text>
+                <View style={ styles.dialogBottomContainer }>
+                  <View style={ styles.leftButtonWrapper }>
+                    <TouchableOpacity activeOpacity={ .5 } onPress={ () => this.onCloseGym() }>
+                      <Text style={ styles.button }>NO</Text>
+                    </TouchableOpacity>
+                  </View>
+                  <View style={ styles.rightButtonWrapper }>
+                    <TouchableOpacity activeOpacity={ .5 } onPress={ () => this.onSetGym() }>
+                      <Text style={ styles.button }>YES</Text>
+                    </TouchableOpacity>
+                  </View>
+                </View>
+              </View>
+              <View style={ styles.dialogTopContainer}>
+                <Image source={ pin_gym } style={ styles.gymBanner } />
+              </View>
+            </View>
+          </PopupDialog>
+
         </Image>
       </View>
     );
@@ -198,6 +260,63 @@ const styles = StyleSheet.create({
   },
   map: {
     flex: 1,
-  }
+  },
+  dialogContainer: {
+    backgroundColor: 'transparent',
+    marginTop: 150,
+  },
+  dialogMainContentContainer: {
+    flex: 1,
+    backgroundColor: 'transparent',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  dialogTopContainer: {
+    width: width * 0.8,
+    position: 'absolute',
+    top: 0,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  gymBanner: {
+    width: 44,
+    height: 44,
+  },
+  dialogContentContainer: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#fff',
+    paddingTop: 22,
+    position: 'absolute',
+    top: 22,
+    width: width * 0.8,
+    borderRadius: 20,
+  },
+  dialogText: {
+    paddingHorizontal: 30,
+    paddingVertical: 20,
+  },
+  dialogBottomContainer: {
+    flexDirection: 'row',
+    borderTopWidth: 1,
+    borderTopColor: '#e6e6e6',
+  },
 
+  leftButtonWrapper: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  rightButtonWrapper: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderLeftWidth: 1,
+    borderLeftColor: '#e6e6e6',
+    height: 40,
+
+  },
+  button: {
+    textAlign: 'center',
+  },
 });
