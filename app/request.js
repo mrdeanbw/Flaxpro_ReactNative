68bfee5f42b44d7ca9f15f5f8c3c2a535cc04eae
VadimAlliance <vadim.kholodov@dunicealliance.com>
2017-05-12 09:08:35 +0300

    added request file

---
+++ app/request.js
@@ -0,0 +1,73 @@
+import 'whatwg-fetch';
+
+/**
+ * Parses the JSON returned by a network request
+ *
+ * @param  {object} response A response from a network request
+ *
+ * @return {object}          The parsed JSON from the request
+ */
+function parseJSON(response) {
+  return response.json();
+}
+
+/**
+ * Checks if a network request came back fine, and throws an error if not
+ *
+ * @param  {object} response   A response from a network request
+ *
+ * @return {object|undefined} Returns either the response, or throws an error
+ */
+function checkStatus(response) {
+  console.log('checkStatus', response);
+  if (response.status >= 200 && response.status < 300) {
+    return response;
+  }
+
+  const error = new Error(response.statusText);
+  error.response = response;
+  throw error;
+}
+
+/**
+ * Requests a URL, returning a promise
+ *
+ * @param  {string} url       The URL we want to request
+ * @param  {object} [options] The options we want to pass to "fetch"
+ *
+ * @return {object}           The response data
+ */
+export default function request(url, options, authState) {
+  const apiUrl = 'http://192.168.88.56:3000/api';
+  const headers = {
+    Accept: 'application/json',
+    // 'Content-Type': 'application/x-www-form-urlencoded',
+    'Content-Type': 'application/json',
+  };
+
+  /**
+   * Will be used with Authorization
+   */
+  // if (authState && authState.token && authState.token.token) {
+  //   headers.Authorization = `Bearer ${authState.token.token}`;
+  // }
+
+  const requestOptions = {
+    ...options,
+    headers: { ...options.headers, ...headers },
+  };
+
+  const result = fetch(apiUrl + url, requestOptions)
+    .then(checkStatus)
+    .then(parseJSON);
+
+  return result;
+}
+
+export function toQueryString(paramsObject) {
+  return Object
+    .keys(paramsObject)
+    .map((key) => `${encodeURIComponent(key)}=${encodeURIComponent(paramsObject[key])}`)
+    .join('&')
+    ;
+}
