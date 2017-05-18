058a702ee417792a2fafa63ed785afc8ad2a9fe7
VadimAlliance <vadim.kholodov@dunicealliance.com>
2017-05-18 12:53:43 +0300

    fixed alert

--- app/Auth/components/smart/authForm.js
+++ app/Auth/components/smart/authForm.js
@@ -55,10 +55,10 @@ class AuthForm extends Component {
 
   componentWillReceiveProps(nextProps) {
     const { auth: { user, error, token } } = nextProps;
-    // if (error) {
-    //   Alert.alert(error);
-    //   return;
-    // }
+    if (error) {
+      Alert.alert(error);
+      return;
+    }
     AsyncStorage.setItem('token', token);
     if (user && this.state.loginRequest) {
         Actions.Main({ user_mode: user.role })
