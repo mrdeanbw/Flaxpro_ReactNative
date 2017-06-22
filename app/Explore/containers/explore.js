8287014c8f00b667c93d115205a997467fc80f5e
VadimAlliance <vadim.kholodov@dunicealliance.com>
2017-06-22 13:48:45 +0300

    fix explore tab for client and fix filter for professions

--- app/Explore/containers/explore.js
+++ app/Explore/containers/explore.js
@@ -4,12 +4,17 @@ import React from 'react';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 import ExploreForm from '../components/smart/exploreForm';
+import ClientExploreForm from '../components/smart/exploreForm/clientExploreForm';
 import * as exploreActions from '../actions';
+import * as CommonConstant from '../../Components/commonConstant';
 
 const Explore = (props) => {
   const { actions, auth, explore } = props;
   return (
-    <ExploreForm { ...actions } auth={ auth } explore={ explore }/>
+    auth.user.role === CommonConstant.user_client ?
+      <ClientExploreForm { ...actions } auth={ auth } explore={ explore }/>
+      :
+      <ExploreForm { ...actions } auth={ auth } explore={ explore }/>
   );
 };
 
